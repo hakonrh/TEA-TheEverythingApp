ff99078ee2cc0a4a7b3320bc35086b762e3a655d
hakonrh <hukonriisu@gmail.com>
2025-03-22 00:23:52 +0100

    remove if statement

--- tea-backend/start.sh
+++ tea-backend/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Run Alembic migrations (if using)
-if [ -f "alembic.ini" ]; then
-  alembic upgrade head
-fi
-
-# Start FastAPI with Uvicorn
+alembic upgrade head
 uvicorn main:app --host 0.0.0.0 --port 10000
