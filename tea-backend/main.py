a9fa51676e5671246feb4a8eff0d17b770623cff
hakonrh <hukonriisu@gmail.com>
2025-03-23 00:43:23 +0100

    import router differently correctly now it will work now for sure now

--- tea-backend/main.py
+++ tea-backend/main.py
@@ -3,13 +3,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 from database import get_db
 
-from routers import insert_dummy_data, fetch_tables, user_registration
+from routers import insert_dummy_data, fetch_tables
+from routers.user_registration import router as user_router
 
 app = FastAPI()
 
 app.include_router(insert_dummy_data.router, prefix="/data")
 app.include_router(fetch_tables.router)
-app.include_router(user_registration, prefix="/user")
+app.include_router(user_router, prefix="/user")
 
 @app.get("/")
 async def root():
