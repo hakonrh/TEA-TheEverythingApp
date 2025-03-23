b718b6845b68ff4bd0e5f561815c33c6e8a2fd23
hakonrh <hukonriisu@gmail.com>
2025-03-23 21:04:39 +0100

    forgot to update routes in main

--- tea-backend/main.py
+++ tea-backend/main.py
@@ -3,13 +3,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 from database import get_db
 
-from routers import insert_dummy_data, fetch_tables
+from routers import fetch_tables, posts, accounts
 from routers.user_registration import router as user_router
 
 app = FastAPI()
 
-app.include_router(insert_dummy_data.router, prefix="/data")
 app.include_router(fetch_tables.router)
+app.include_router(posts.router)
+app.include_router(accounts.router)
 app.include_router(user_router, prefix="/user")
 
 @app.get("/")
